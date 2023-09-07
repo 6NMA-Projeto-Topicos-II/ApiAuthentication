1d3de1ac5aec98a6109dee46028d7ea5c0d19e12
renzotakada <renzotakada1@gmail.com>
2023-09-07 17:40:41 -0300

    feat: Endpoint Cadastra Usuario

---
+++ Api-Authentication/Adapters/Http/EndpointCadastrar.cs
@@ -0,0 +1,15 @@
+﻿using Api_Authentication.Domain.DTO;
+
+namespace Api_Authentication.Adapters.Http
+{
+    public static class EndpointCadastrar
+    {
+        public static void AddEndpointCadastro(this WebApplication app)
+        {
+            app.MapPost("/v1/Cadastrar", async Task<IResult> (ResquestCadastrarUsuario request) =>
+            {
+                return Results.Ok(request);
+            });
+        }
+    }
+}
---
+++ Api-Authentication/Domain/DTO/ResquestCadastrarUsuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Api_Authentication.Domain.DTO
+{
+    public struct ResquestCadastrarUsuario
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Matricula { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
