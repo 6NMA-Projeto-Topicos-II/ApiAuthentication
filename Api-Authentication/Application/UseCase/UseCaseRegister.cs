383be59f63676e3223a46a4d8ab5767f9e3752bb
renzotakada <renzotakada1@gmail.com>
2023-09-07 17:59:12 -0300

    feat: construindo useCase Registrar

---
+++ Api-Authentication/Application/UseCase/UseCaseRegister.cs
@@ -0,0 +1,23 @@
+﻿using Api_Authentication.Domain.DTO;
+using Api_Authentication.Port.InputboundPort;
+using Api_Authentication.Port.OutboundPort;
+
+namespace Api_Authentication.Application.UseCase
+{
+    public class UseCaseRegister : IUseCaseRegister
+    {
+        private readonly IRepositoryRegister _repository;
+        public UseCaseRegister(IRepositoryRegister repository)
+        {
+            _repository = repository;
+        }
+        public Task<string> Execute(InputRegisterUser request)
+        {
+            var retRepository = await _repository.QueryUsers(request);
+            if (retRepository. == null)
+            {
+
+            }
+        }
+    }
+}
