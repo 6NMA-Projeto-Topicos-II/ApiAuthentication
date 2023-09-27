e901ea49abc49b152c508cfddcb5d4694be1dea4
RENZO TAKADA <63244432+RenzoTakada@users.noreply.github.com>
2023-09-26 22:11:21 -0300

    Update ScriptTabelaUsuarios.sql

--- ScriptTabelaUsuarios.sql
+++ ScriptTabelaUsuarios.sql
@@ -13,11 +13,12 @@ BEGIN
     CREATE TABLE DbCampusHub.dbo.tbl_Usuarios
     (
 		Matricula VARCHAR(255) PRIMARY KEY ,
-        Nome NVARCHAR(255) NOT NULL,
-		Sobrenome NVARCHAR(255) NOT NULL,
-		Email NVARCHAR(255),
-		Senha NVARCHAR(255) NOT NULL,
-		DataCriacao DATETIME NOT NULL
+        Nome VARCHAR(255) NOT NULL,
+		Sobrenome VARCHAR(255) NOT NULL,
+		Email VARCHAR(255),
+		Senha VARCHAR(255) NOT NULL,
+		DataCriacao VARCHAR(255) NOT NULL
     );
+
     PRINT 'Tabela criada com sucesso.'
 END
